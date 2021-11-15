d5e70e2d31dbd8f7d8e18aeb7e78f1ae70da8045
Alper Yetik <76109274+Alper-Yetik@users.noreply.github.com>
2021-11-15 23:25:39 +0300

    40.Ders
    40. Ders Son Hali

---
+++ MvcProjeKampi/Controllers/IstatistikController.cs
@@ -0,0 +1,21 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcProjeKampi.Controllers
+{
+    public class IstatistikController : Controller
+    {
+        // GET: Istatistik
+        CategoryManager cm = new CategoryManager(new EfCategoryDal());
+        public ActionResult Index()
+        {
+            var categoryvalues = cm.GetList();
+            return View(categoryvalues);
+        }
+    }
+}
